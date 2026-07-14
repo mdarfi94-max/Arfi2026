01f34d36a7d37e04d4963c15b5606409cd6caf00
Hp <Hp@DESKTOP-1RHLS1L.bbrouter>
2026-07-14 15:00:42 +0530

    this is my first commit

---
+++ SpringBoot-REST-FirstApp/src/main/java/com/in/SpringBootRestFirstAppApplication.java
@@ -0,0 +1,13 @@
+package com.in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestFirstAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestFirstAppApplication.class, args);
+	}
+
+}
---
+++ SpringBoot-REST-FirstApp/src/main/java/com/in/controller/EmployeeRestController.java
@@ -0,0 +1,20 @@
+package com.in.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/emp")
+public class EmployeeRestController {
+
+	@GetMapping("/show")
+	public ResponseEntity<String> show(){
+		String body="this is Spring Rest App";
+		ResponseEntity<String> res=new ResponseEntity<String>(body,HttpStatus.OK);
+		return res;	
+	}
+	
+}
